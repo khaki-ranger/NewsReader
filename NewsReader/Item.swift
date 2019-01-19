e421322150d33f89225f30c20e3b267a3ce82698
Terashima <yamanogomora@gmail.com>
2019-01-19 15:02:04 +0900

    RSSデータの取得と解析

---
+++ NewsReader/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  NewsReader
+//
+//  Created by 寺島 洋平 on 2019/01/19.
+//  Copyright © 2019年 YoheiTerashima. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title = ""
+    var link = ""
+}
